2e556052281a39da5ae986508f0c41485d81c23f
Prateek Adhikari <prateekadhikari@prateeks-MacBook-Pro.local>
2026-07-05 21:01:50 +0530

    nothing

--- app.js
+++ app.js
@@ -1,55 +1,57 @@
-// let color = "red"
+let color = "red"
 
-// switch(color){
-//     case "red":
-//         console.log(stop)
-//         break;
-//     case "yellow":
-//         console.log(wait)
-//         break;
-//     case "green":
-//         console.log("go")
-//         break;
-//     case "default":
-//         console.log("the light is broken")
-//          break;
-//     }
+switch(color){
+    case "red":
+        console.log(stop)
+        break;
+    case "yellow":
+        console.log(wait)
+        break;
+    case "green":
+        console.log("go")
+        break;
+    case "default":
+        console.log("the light is broken")
+         break;
+    }
     
 
-// let name= prompt ("type your first name");
-// let lastname=prompt("write your last name");
-// console.log(`hi $(name) &(lastname) welcome!);
+let name= prompt ("type your first name");
+let lastname=prompt("write your last name");
+console.log(`hi $(name) &(lastname) welcome!);
 
-// alert("Hey Mr. recruiter most welcome!!!!");
+alert("Hey Mr. recruiter most welcome!!!!");
 
-// let i=1
+let i=1
 
-// while(i<10){
-//     console.log(i);
-//     i++
-// }
+while(i<10){
+    console.log(i);
+    i++
+}
 
-
-
-// let firstname =prompt("write your first name");
-// let lastname=prompt("write your last name");
-// console.log(`welcome ${firstname} ${lastname}`);
+let firstname =prompt("write your first name");
+let lastname=prompt("write your last name");
+console.log(`welcome ${firstname} ${lastname}`);
 
 
-// let str="   hello    ";
+let str="   hello    ";
 
 
-// console.log(str.trim());
+console.log(str.trim());
 
 
 
-// for loops
+for loops
 
-// for (let i=1; i<=5; i++){
-//     console.log(i);
+for (let i=1; i<=5; i++){
+    console.log(i);
 
-// }
+}
 let i=1;
-while (i>=5)
+while (i>=5){
+
 console.log(i)
 i++
+
+
+};
